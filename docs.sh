21504285b686f860b98ca8fe4bf9fa7ed97eff99
Francois LaBerge <FGCRLaBerge@gmail.com>
2026-05-21 11:57:41 -0400

    move to a cargo-only implementation

--- docs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-delimiter=" -- "
- man $(apropos . | docs-parse "$delimiter" | fzf --style=full --delimiter="${delimiter}" --accept-nth=1 --with-nth="{1} - {2}" --preview="man --warnings=!font {1}")
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
-use std::{ env, io::{BufRead, stdin}};
+use std::{ io::{ Write}, process::{ChildStdin, Command, Stdio}, };
+
 use regex::Regex;
 
-fn main() -> Result<(), std::io::Error> {
-    let args: Vec<String> = env::args().collect();
+fn main() {
+    let delimiter = " -- ";
+
+    let apropos = Command::new("apropos")
+        .arg(".")
+        .output()
+        .expect("failed to run apropos");
+
+    let mut fzf_process = Command::new("fzf")
+        .args([
+            "--style", "full",
+            "--delimiter", delimiter,
+            "--accept-nth", "1",
+            "--with-nth", "{1} - {2}",
+            "--preview", "man --warnings=!font {1}",
+        ])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to run fzf");
+
+    parse(String::from_utf8(apropos.stdout).expect("failed to read the apropos output"), fzf_process.stdin.take().expect("failed to take the fzf stdin"), delimiter);
+    let fzf = fzf_process.wait_with_output().expect("failed to run fzf");
+    let selection = String::from_utf8(fzf.stdout).expect("failed to read the fzf output");
+    let page = selection.trim();
+
+    let _ = Command::new("man")
+        .args([page])
+        .status()
+        .expect("failed to run man");
+}
+
+fn parse(apropos: String, mut fzf: ChildStdin, delimiter: &str) {
     let regex = Regex::new(r"([^\s^\)]+)\s*\(([^\(]+)\)\s*-\s(.*)").expect("Could not parse regex");
-
-    let stdin: Vec<String> = stdin().lock().lines().into_iter().map(
-        |line| line.unwrap()
-    ).collect();
-
-    let hay = stdin.join("\n");
-
-    for (_, [tool, _number, description]) in regex.captures_iter(hay.as_str()).map(|c| c.extract()) {
-        println!("{}{}{}", tool, args[1], description)
+    for (_, [tool, _number, description]) in regex.captures_iter(apropos.as_str()).map(|c| c.extract()) {
+        // std::thread::spawn( |fzf| {
+        let _ = fzf.write_all(format!("{}{}{}\n", tool, delimiter, description).as_bytes());
+        // });
     }
-
-    return Ok(());
 }
